2be7f518bd4f3058c3b372460f9e3e614164a925
Aleksandr Vladimirovich Nikonov <alex_basic@mail.ru>
2022-04-22 17:13:47 +0500

    fix one bag in the listview grouping

--- Ru.Mail.AlexBasic.GUIPasswordManager/Controls/ListViewAdd.cs
+++ Ru.Mail.AlexBasic.GUIPasswordManager/Controls/ListViewAdd.cs
@@ -19,21 +19,26 @@ namespace Ru.Mail.AlexBasic.GUIPasswordManager
 
         public void SetValues(IEnumerable<SecretsListItem> secrets, IEnumerable<GroupListView> groups)
         {
-            BeginUpdate();
-            Items.Clear();
-            Groups.Clear();
-            foreach (var group in groups)
+            try
             {
-                Groups.Add(group.ListViewGroup);
+                BeginUpdate();
+                Items.Clear();
+                Groups.Clear();
+                foreach (var group in groups)
+                {
+                    Groups.Add(group.Name, group.Name);
+                }
+                foreach (var secret in secrets)
+                {
+                    var secretGroup = Groups[secret.GroupName];
+                    secret.Group = secretGroup;
+                    Items.Add(secret);
+                }
             }
-            foreach (var secret in secrets)
+            finally
             {
-                var secretGroup = Groups[secret.Name];
-                secret.Group = secretGroup;
-                Items.Add(secret);
+                EndUpdate();
             }
-
-            EndUpdate();
         }
 
         public class GroupListView
@@ -56,52 +61,17 @@ namespace Ru.Mail.AlexBasic.GUIPasswordManager
         public class SecretsListItem : ListViewItem
         {
             public int Id { get; set; }
-            public string NameOfSecret { get; set; }
-            public string Value { get; set; }
-            public string Comment { get; set; }
-            public int GroupId { get; set; }
             public string GroupName { get; set; }
-
-            public ListViewGroup ListViewGroup { get; set; }
 
             public SecretsListItem(int id, string name, string value, string comment, int groupId, string groupName)
             {
                 Id = id;
-                Value = value;
-                Comment = comment;
-                GroupId = groupId;
                 GroupName = groupName;
-
                 Text = name;
                 Name = name + "ListViewItem";
                 SubItems.Add(value);
                 SubItems.Add(comment);
-            }
-        }
-
-        public class ListViewGroupSorter : IComparer
-        {
-            private SortOrder order;
-
-            public ListViewGroupSorter(SortOrder theOrder)
-            {
-                order = theOrder;
-            }
-
-            public int Compare(object x, object y)
-            {
-                int result = String.Compare(
-                    ((ListViewGroup)x).Header,
-                    ((ListViewGroup)y).Header
-                );
-                if (order == SortOrder.Ascending)
-                {
-                    return result;
-                }
-                else
-                {
-                    return -result;
-                }
+                SubItems.Add(groupName);
             }
         }
     }
