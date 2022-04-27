bd326fe4c100dcd6f1c6722cc7b17179bd666fa7
Aleksandr Vladimirovich Nikonov <alex_basic@mail.ru>
2022-04-27 11:57:23 +0500

    added GPLv3 license

---
+++ Ru.Mail.AlexBasic.GUIPasswordManager.Test/CryptoProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Ru.Mail.AlexBasic.GUIPasswordManager.Infrastructure.Crypto;
+
+namespace Ru.Mail.AlexBasic.GUIPasswordManager.Test
+{
+    public class CryptoProviderTest
+    {
+        [Test]
+        public void TheExampleOfProtection()
+        {
+            var provider = new CryptoProvider();
+
+            var stringForEncode = "Hello world! Hello world! Hello world! Hello world! Hello world!";
+            var password = "somePassword";
+
+            var encodedData = provider.Encode(stringForEncode, password);
+            provider.Decode();
+
+            Assert.Pass();
+        }
+    }
+}
